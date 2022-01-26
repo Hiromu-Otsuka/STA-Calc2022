bebd9a8cc83cd62393103d750f0abc2609f568fd
n1200450 <caho1200450@gn.iwasaki.ac.jp>
2022-01-26 22:25:39 +0900

    2022/01/26 22:00

--- Calculator.js
+++ Calculator.js
@@ -1,23 +1,44 @@
-var Char = [["(", ")", "^", "C/AC"],
-["7", "8", "9", "÷"],
-["4", "5", "6", "×"],
-["1", "2", "3", "-"],
-["0", ".", "=", "+"]];
-var Type = [["OpenBracketes", "CloseBracketes", "Symbol", "Delete"],
-["Number", "Number", "Number", "Symbol"],
-["Number", "Number", "Number", "Symbol"],
-["Number", "Number", "Number", "Symbol"],
-["Number", "Point", "Equal", "Symbol"]];
-var n = 0;
+var Ary = [
+    [
+        {Char:"(",Btn:"(",Type:"Bracketes"},
+        {Char:")",Btn:")",Type:"Bracketes"},
+        {Char:"^",Btn:"$$x^n$$",Type:"Operator"},
+        {Char:"",Btn:"C/AC",Type:"Delete"}
+    ],
+    [
+        {Char:"7",Btn:"7",Type:"Number"},
+        {Char:"8",Btn:"8",Type:"Number"},
+        {Char:"9",Btn:"9",Type:"Number"},
+        {Char:"/",Btn:"÷",Type:"Operator"}
+    ],
+    [
+        {Char:"4",Btn:"4",Type:"Number"},
+        {Char:"5",Btn:"5",Type:"Number"},
+        {Char:"6",Btn:"6",Type:"Number"},
+        {Char:"*",Btn:"×",Type:"Operator"}
+    ],
+    [
+        {Char:"1",Btn:"1",Type:"Number"},
+        {Char:"2",Btn:"2",Type:"Number"},
+        {Char:"3",Btn:"3",Type:"Number"},
+        {Char:"-",Btn:"-",Type:"Operator"}
+    ],
+    [
+        {Char:"0",Btn:"0",Type:"Number"},
+        {Char:".",Btn:".",Type:"Point"},
+        {Char:"=",Btn:"=",Type:"Equal"},
+        {Char:"+",Btn:"+",Type:"Operator"}
+    ],
+];
 var FromString, Text2, Text1 = "";
 
-function CreateTable() {
+window.onload = function CreateTable() {
     var Table = document.createElement("table");
-    for (var i = -2; i < Char.length; i++) {
+    for (var i = -2; i < Ary.length; i++) {
         var Row = document.createElement("tr");
         if (i < 0) {
             var Column = document.createElement("th");
-            Column.setAttribute("colspan", Char[0].length);
+            Column.setAttribute("colspan", Ary[0].length);
             Column.setAttribute("class", "cell display");
             switch (i) {
                 case -2:
@@ -29,19 +50,20 @@ function CreateTable() {
             }
             Row.appendChild(Column);
         } else {
-            for (var j = 0; j < Char[0].length; j++) {
+            for (var j = 0; j < Ary[i].length; j++) {
                 var Column = document.createElement("td");
-                Column.textContent = Char[i][j];
-                Column.setAttribute("id", "Button" + i + "-" + j);
+                Column.insertAdjacentHTML("beforeend", Ary[i][j].Btn);
                 Column.setAttribute("onclick", "TypeBotton(" + i + "," + j + ")");
-                Column.setAttribute("ondblclick", "AllClear()");
-                if (Type[i][j] == "Number") {
+                if (Ary[i][j].Type == "Number") {
                     Column.setAttribute("class", "cell number");
-                } else if (Type[i][j] == "Delete") {
+                } else if (Ary[i][j].Type == "Delete") {
                     Column.setAttribute("class", "cell delete");
-                } else if (Type[i][j] == "Equal") {
+                    Column.setAttribute("ondblclick", "AllClear()");
+                } else if (Ary[i][j].Type == "Equal") {
                     Column.setAttribute("class", "cell equal");
-                } else {
+                } else if (Ary[i][j].Type == "Point") {
+                    Column.setAttribute("class", "cell point");
+                }else {
                     Column.setAttribute("class", "cell");
                 }
                 Row.appendChild(Column);
@@ -56,7 +78,7 @@ function TypeBotton(x, y) {
     var GetText1 = document.getElementById("Text1");
     var GetText2 = document.getElementById("Text2");
     Text2 = GetText2.textContent;
-    var CurrentType = Type[x][y];
+    var CurrentType = Ary[x][y].Type;
     if (CurrentType == "Equal") {
         FromString = Text2.replace(/×/g, "*");
         FromString = FromString.replace(/÷/g, "/");
@@ -67,11 +89,11 @@ function TypeBotton(x, y) {
     } else if (CurrentType == "Delete") {
         GetText2.textContent = Text2.slice(0, -1);
     } else {
-        GetText2.textContent += Char[x][y];
+        GetText2.textContent += Ary[x][y].Char;
     }
 }
 
-function AllClear(){
+function AllClear() {
     document.getElementById("Text1").textContent = "";
     document.getElementById("Text2").textContent = "";
 }
